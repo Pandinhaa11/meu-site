5941717634f500e3d3bd4b3b1bce55ef41d07eab
Pandinhaa11 <maraujosoares936@gmail.com>
2024-11-16 16:57:00 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import logo from './logo_site2.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+          ~~ Site do P ~~
+        
+
+        <p>
+            <code>
+
+            ⠀
+            </code>
+            </p>
+
+        <img src={logo} className="App-logo" alt="logo" />
+
+        <p>
+            <code>
+
+            ⠀
+            </code>
+            </p>
+
+            <p>
+              <h1> Bem vindo ao meu site!</h1>
+              <h2>~~BIOGRAFIA~~</h2>
+              <h4>Um carinha que gosta de programar, jogar e conversar! Se caso vc quer conversar com ele, </h4>
+              <h4>vá até a seção "~~COMO CONTATAR~~" :3</h4>
+              <h2>~~PROJETOS~~</h2>
+              <h4>Não implantado no site ainda, triste :(</h4>
+              <h2>~~COMO CONTATAR~~</h2>
+              <h4>Discord: meggupoido</h4>
+              <h4>Email: maraujosoares936@gmail.com</h4>
+              <h4>Email para solicitação de projetos: kuririn.uwu.66@gmail.com</h4>
+              <h4>Outros em breve! :3</h4>
+              </p>
+
+              <h6>Copyright © Panda Oficial 2024</h6>
+
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
